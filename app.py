7aacf2ba2c68f6d28d7db2e4658c9c0fbb363ed6
NandVinchhi <nand.vinchhi@gmail.com>
2020-08-13 08:59:42 +0530

    init

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def getupload():
             size = (224, 224)
             image = ImageOps.fit(image, size, Image.ANTIALIAS)
             image_array = np.asarray(image)
-            # image_array = np.stack((image_array,)*3, axis=-1)
+            image_array = np.stack((image_array,)*3, axis=-1)
             normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
             
             data[0] = normalized_image_array
@@ -191,7 +191,7 @@ def getupload():
             return render_template('results.html', diagnosis=diagnosis, prob=prob, result=result, text=text)
             
         else:
-            return render_template('upload.html', errorMessage="Please upload either a jpeg or png image. AA")
+            return render_template('upload.html', errorMessage="Please upload either a jpeg or png image.")
         
     return redirect("/upload")
 
